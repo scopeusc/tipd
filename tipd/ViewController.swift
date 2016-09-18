4356c20e23e0d533f0a36adbbb8d2af7ac489c51
Stacey Irawan <sirawan@usc.edu>
2016-09-18 16:33:35 -0700

    Updated files

--- tipd/ViewController.swift
+++ tipd/ViewController.swift
@@ -19,7 +19,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBAction func onTap(sender: AnyObject) {
+        
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(sender: AnyObject) {
+        let tipPercentages = [0.15, 0.2, 0.25]
+        
+        
+        let bill = Double(billField.text!) ?? 0
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f",tip)
+        totalLabel.text = String(format: "$%.2f",total)
 }
-
+}
